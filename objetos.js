79521a566e1a785578e13eac84ebdd4eb649692f
SAH <143020123+BRINAHH@users.noreply.github.com>
2024-04-30 11:31:08 -0300

    Add files via upload

---
+++ objetos.js
@@ -0,0 +1,16 @@
+class Calebe {
+    constructor (site) {
+        this.site = site //variavel de interface
+        //this.parametro2 = parametro2 //variavel de interface
+        //açoes
+    }
+
+    exibirSite () {
+        //açoes
+        window.open(this.site);
+    }
+
+    metodo2 () {
+        //açoes
+    }
+}
